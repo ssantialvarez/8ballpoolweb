b767d54b6360164092278f50ac3f49729edea744
Santiago Alvarez <alvarezsanjavier@gmail.com>
2025-11-07 18:08:13 -0300

    FIX: procy does not use request

--- proxy.ts
+++ proxy.ts
@@ -16,7 +16,7 @@ export async function proxy(request: NextRequest) {
 
   // Any route that gets to this point will be considered a protected route, and require the user to be logged-in to be able to access it
   const { origin } = new URL(request.url)
-  const session = await auth0.getSession(request)
+  const session = await auth0.getSession()
 
   // If the user does not have a session, redirect to login
   if (!session) {
